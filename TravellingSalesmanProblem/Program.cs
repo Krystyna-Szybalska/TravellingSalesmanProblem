3fee14e183cf24a54a44424645a42e3ce06d2afe
Krystyna.Szybalska <Krystyna.Szybalska@PK2-11>
2022-06-04 14:15:53 +0200

    Dodaj pliki projektów.

---
+++ TravellingSalesmanProblem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TravellingSalesmanProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //koszt jest dodatni, naturalny
+            //komiwojażer musi odwiedzić wszystkie miasta dokładnie raz
+            //wybór najtańszej ścieżki
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
